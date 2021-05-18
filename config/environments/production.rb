38f69b90b1e5e593387ab9b8a277f79e87da91f2
patrick-angelos <angelosp07@gmail.com>
2021-05-18 14:08:07 +0300

    Add host url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,3 +118,5 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 end
+
+Rails.application.routes.default_url_options = { host: "https://hidden-wildwood-65842.herokuapp.com/" }
